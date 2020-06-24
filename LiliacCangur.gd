76fb6adc5f0bd7b0af3508d192b894e5d438d4d9
Cruceru Nicusor <crucerunicusor04@gmail.com>
2020-06-24 16:41:42 +0300

    Inital commit

---
+++ LiliacCangur.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+const GRAVITY = 150 
+const FLY_FORCE = 5000 
+const FLY_FORCE_X = 3000
+
+var motion = Vector2.ZERO
+
+var jump_input = false
+
+func _ready():
+	pass	
+	
+func _process(delta):
+	$Sprite/AnimationPlayer.play("Fly")
+	
+	$CanvasLayer/RichTextLabel.text = "Score: %d" % (global_position.x / 200)
+	
+func _physics_process(delta):
+	
+	jump_input = Input.is_action_just_pressed("ui_accept")
+	
+	if jump_input: 
+		motion.y = - FLY_FORCE * delta
+		motion.x = FLY_FORCE_X * delta
+	
+	motion.y += GRAVITY * delta 
+	
+	motion = move_and_slide(motion)
+	
+
+
+func _on_Area2D_body_entered(body):
+	if body.name != "LiliacCangur":
+		pass	
+#		get_tree().paused  = true
+#		$CanvasLayer/RichTextLabel2.visible = true
---
+++ World.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var last_position = 0
+
+var stalp = load("Stalp.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if $LiliacCangur.global_position.x > last_position + 200:
+		last_position = $LiliacCangur.global_position.x
+		var stalp_1 = stalp.instance()
+		var stalp_2 = stalp.instance()
+		stalp_1.set_position(Vector2(last_position + 200, 20.6))
+		stalp_2.set_position(Vector2(last_position + 200, 265.0))
+
+		stalp_2.global_rotation = stalp_1.global_rotation + PI 
+		add_child(stalp_1)
+		add_child(stalp_2)
+		print("HAI cu tata")
+	
